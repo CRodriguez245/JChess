9771293dbec600c1391d4604f7ea9d86ecbde260
carlosrodriguez <c.a.rodriguez1999@gmail.com>
2018-12-24 15:16:26 -0600

    Chess Game

---
+++ src/com/chess/engine/player/ai/MoveStrategy.java
@@ -0,0 +1,11 @@
+package com.chess.engine.player.ai;
+
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Board;
+
+public interface MoveStrategy {
+
+    Move execute(Board board);
+
+
+}
